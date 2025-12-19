f8c57791d0011d0007315bd16622bda9c2aea30e
Sara Jamous <129776424+sarajamous@users.noreply.github.com>
2025-12-19 02:26:12 +0200

    Update Numpy_Package/numpypackage_practice.py
    Co-authored-by: Alex Sato <87045798+qwriter@users.noreply.github.com>

--- Numpy_Package/numpypackage_practice.py
+++ Numpy_Package/numpypackage_practice.py
@@ -17,7 +17,7 @@ itinerary = D[x[:-1], x[1:]]
 # Print the array itinerary
 print("The distances between consecutive locations, excluding the return trip to the origin is:", itinerary)
 
-#TODO: Compute the total distance traveled between consecutive locations, excluding return trip to the origin. 
+#TODO: Compute the total distance traveled between consecutive locations, excluding the return trip to the origin. 
 # Hint: Use the .sum() method
 
 
